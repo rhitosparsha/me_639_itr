75f5fbb64b631ddff93145658ee8b7007e161637
rhitosparsha <141488762+rhitosparsha@users.noreply.github.com>
2023-09-01 02:12:35 +0530

    Add files via upload

---
+++ assignment_2/question_4.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+"""
+Program to calculate the end effector position vector for an RRP Stanford robot.
+
+Parameters:
+q1 =  Angle of Joint 1
+q2 =  Angle of Joint 2
+l1 = Length of Link 1
+l2 = Length of Link 2
+l3 = Length of link 3
+d3 = Linear displacement
+"""
+
+def stanford_fwd_kin(q1, q2, l1, l2, l3, d3):
+    P3 = np.array([l3, 0, 0, 1])
+    
+    H_01 = np.array([
+                        [np.cos(q1), np.sin(q1), 0, 0],
+                        [-np.sin(q1), np.sin(q1), 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]
+                    ]) 
+    
+    H_12 = np.array([
+                        [np.cos(q2), -np.sin(q2), 0, 0],
+                        [0, 0, -1, 0],
+                        [np.sin(q2), np.cos(q2), 0, l1],
+                        [0, 0, 0, 1]
+                    ])
+
+    H_23 = np.array([
+                        [1, 0, 0, l2 + d3],
+                        [0, 1, 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]
+                    ])   
+    
+    P0 = np.dot(np.dot(np.dot(H_01, H_12), H_23), P3) 
+
+    return P0
+
+l1 = float(input("Enter length of Link 1 (m): "))
+l2 = float(input("Enter length of Link 2 (m): "))
+l3 = float(input("Enter length of Link 3 (m): "))
+q1 = np.radians(float(input("Enter angle of Joint 1 (deg): ")))
+q2 = np.radians(float(input("Enter angle of Joint 2 (deg): ")))
+d3 = float(input("Enter extension length (m): "))
+
+pos = stanford_fwd_kin(q1, q2, l1, l2, l3, d3)
+
+print("\nEnd effector position :")
+print("x = ", str(pos[0])," m, y = ",str(pos[1]), " m, z = ", str(pos[2]), " m")
+
+input()
